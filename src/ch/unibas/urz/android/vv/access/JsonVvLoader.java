12162ebeae7533fba532b3a235e26696eb7b43ef
Patrick S. Vogt <patrick.vogt@unibas.ch>
2011-08-19 15:55:51 +0200

    cleanup

--- src/ch/unibas/urz/android/vv/access/JsonVvLoader.java
+++ src/ch/unibas/urz/android/vv/access/JsonVvLoader.java
@@ -81,30 +81,6 @@ public class JsonVvLoader {
 
 	}
 
-	// private static void insertOrUpdate(Context ctx, AppModel am) {
-	// ContentResolver contentResolver = ctx.getContentResolver();
-	// String[] selectionArgs = new String[] { Integer.toString(am.getAppId())
-	// };
-	// Cursor c = contentResolver.query(DB.DashboardApp.CONTENT_URI,
-	// DB.DashboardApp.PROJECTION_DEFAULT, DB.DashboardApp.SELECTION_BY_APPID,
-	// selectionArgs,
-	// DB.DashboardApp.SORTORDER_DEFAULT);
-	// try {
-	// if (c.moveToFirst()) {
-	// Logger.d("update " + am.getName());
-	// am.setDbid(c.getLong(DB.INDEX_ID));
-	// am.setHide(c.getInt(DB.DashboardApp.INDEX_HIDE));
-	// contentResolver.update(DB.DashboardApp.CONTENT_URI, am.getValues(),
-	// DB.DashboardApp.SELECTION_BY_APPID, selectionArgs);
-	// } else {
-	// Logger.d("insert " + am.getName());
-	// contentResolver.insert(DB.DashboardApp.CONTENT_URI, am.getValues());
-	// }
-	// } catch (Throwable t) {
-	// Logger.e("Fail to insert ", t);
-	// }
-	// }
-
 	private static String loadData(String url) throws Exception {
 		URL aUrl = new URL(url);
 		URLConnection conn = aUrl.openConnection();
